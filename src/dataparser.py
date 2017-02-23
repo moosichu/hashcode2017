991a1aa1d2c02b8ac2ac3c1ac91afc849c941444
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2017-02-23 18:39:44 +0000

    Add datastructures

--- src/dataparser.py
+++ src/dataparser.py
@@ -1,1 +1,28 @@
-print("hello world")
+class Data:
+    def __init__(self):
+        self.num_videos = 0
+        self.num_endpoints = 0
+        self.num_requests = 0
+        self.num_caches = 0
+        self.cache_size = 0
+
+        self.video_data = []
+        self.endpoint_data = [] # list of Endpoints
+        self.requests = []
+
+class Endpoint:
+    def __init__(self):
+        """
+        Contains dictionary indexing cacheids to latency
+        """
+        self.num_links = 0
+        self.cache_links = {}
+
+class Reqjest:
+    def __init__(self, num_requests, video_id, endpoint):
+        self.num_requests = num_requests
+        self.video_id = video_id
+        self.endpoint = endpoint
+
+def getData(arg):
+    pass
