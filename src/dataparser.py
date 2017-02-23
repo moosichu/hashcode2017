66ea750517f1c7fa894df1f5fa21cf973f6609c9
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2017-02-23 19:11:20 +0000

    Add output data structures

--- src/dataparser.py
+++ src/dataparser.py
@@ -1,4 +1,5 @@
-class Data:
+# definition of input data structures
+class InputData:
     def __init__(self):
         self.num_videos = 0
         self.num_endpoints = 0
@@ -16,7 +17,7 @@ class Endpoint:
         Contains dictionary indexing cacheids to latency
         """
         self.num_links = 0
-        self.cache_links = {}
+        self.cache_links = {} # dictionary from int to int of cache id to cache latency
 
 class Request:
     def __init__(self, num_requests, video_id, endpoint):
@@ -24,5 +25,12 @@ class Request:
         self.video_id = video_id
         self.endpoint = endpoint
 
-def getData(arg):
-    pass
+# definition of output data structures
+class OutputData:
+    def __init__(self):
+        self.num_used_servers = 0
+        self.cache_to_video = {} # dictionary from cache ids to CacheData
+
+class CacheData:
+    def __init__(self):
+        self.videos = [] # list of video ids used (int)
